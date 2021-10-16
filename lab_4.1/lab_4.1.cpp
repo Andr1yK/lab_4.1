85a1cfe3c2c0905792cb3f5cb4c7f4de8f74db69
Andriy Kobetiak <andriykobetiak@gmail.com>
2021-10-16 16:46:15 +0300

    Добавьте файлы проекта.

---
+++ lab_4.1/lab_4.1.cpp
@@ -0,0 +1,56 @@
+﻿// lab_4.1.cpp
+// Кобетяк Андрій
+// Лабораторна робота № 4.1
+// Цикли
+// Варіант 12
+
+#include <iostream>;
+
+using namespace std;
+
+int main()
+{
+	int i, N;
+	double S;
+
+	cout << "N = "; cin >> N;
+
+	S = 0;
+	i = N;
+
+	while (i <= 22)
+	{
+		S += sqrt(i * i + N * N) / i;
+		i++;
+	}
+	cout << "S = " << S << " (while)" << endl;
+
+	S = 0;
+	i = N;
+
+	do
+	{
+		S += sqrt(i * i + N * N) / i;
+		i++;
+	} while (i <= 22);
+	cout << "S = " << S << " (do ... while)" << endl;
+
+	S = 0;
+	i = N;
+
+	for (int i = N; i <= 22; i++)
+	{
+		S += sqrt(i * i + N * N) / i;
+	}
+	cout << "S = " << S << " (for i++)" << endl;
+
+	S = 0;
+
+	for (int i = 22; i >= N; i--)
+	{
+		S += sqrt(i * i + N * N) / i;
+	}
+	cout << "S = " << S << " (for i--)" << endl;
+
+	return 0;
+}
